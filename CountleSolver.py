920ec0cf7bc11c5acc5a4d5e6c848471406240b9
Allen <adsteinberg01@gmail.com>
2023-08-26 16:12:24 -0500

    Modified program to find a solution which takes the fewest steps

--- CountleSolver.py
+++ CountleSolver.py
@@ -1,12 +1,15 @@
 def solve(n, sol):
-	if len(n) == 1:
+	if len(n) == 0:
+		return[]
+	elif len(n) == 1:
 		if n[0] == sol:
-			return ["Solved!"]
+			return [["Solved!"]]
 		else:
 			return []
 	elif sol in n:
-		return ["Solved!"]
+		return [["Solved!"]]
 	else:
+		Sret = [] # Set of all solutions to return
 		for i in range(len(n)):
 			for j in range(len(n)):
 				if(j != i):
@@ -22,39 +25,53 @@ def solve(n, sol):
 					n0.append(n[i]+n[j])
 					if(n[i]-n[j] >= 0):
 						n1.append(n[i]-n[j])
+					else:
+						n1 = []
 					n2.append(n[i]*n[j])
 					if(n[j] != 0 and n[i]/n[j] == int(n[i]/n[j])):
 						n3.append(int(n[i]/n[j]))
+					else:
+						n3 = []
 
-					S0 = solve(n0, sol)
-					S1 = solve(n1, sol)
-					S2 = solve(n2, sol)
-					S3 = solve(n3, sol)
+					S = [solve(n0, sol), solve(n1, sol), solve(n2, sol), solve(n3, sol)]
 
-					if(len(S0) != 0):
-						S0.append(str(n[i]) + " + " + str(n[j]) + " = " + str(n[i]+n[j]))
-						return S0
-					elif(len(S1) != 0):
-						S1.append(str(n[i]) + " - " + str(n[j]) + " = " + str(n[i]-n[j]))
-						return S1
-					elif(len(S2) != 0):
-						S2.append(str(n[i]) + " * " + str(n[j]) + " = " + str(n[i]*n[j]))
-						return S2
-					elif(len(S3) != 0):
-						S3.append(str(n[i]) + " / " + str(n[j]) + " = " + str(int(n[i]/n[j])))
-						return S3
-		return []
+					for soln in S[0]:
+						soln.append([n[i], " + ", n[j], " = ", n[i]+n[j]])
+						Sret.append(soln)
+					for soln in S[1]:
+						soln.append([n[i], " - ", n[j], " = ", n[i]-n[j]])
+						Sret.append(soln)
+					for soln in S[2]:
+						soln.append([n[i], " * ", n[j], " = ", n[i]*n[j]])
+						Sret.append(soln)
+					for soln in S[3]:
+						soln.append([n[i], " / ", n[j], " = ", int(n[i]/n[j])])
+						Sret.append(soln)
 
-print("This program solves a countle-type problem. The program does not attempt to find the most efficient solution. Note that if a solution is found, the program may output operations which are unnecessary to arrive at the solution. It may take a few seconds to output its findings.")
+		return Sret
+	
+def find_best_solution(solns):
+	min_length = 10
+	min_index = -1
+	for i in range(len(solns)):
+		if(len(solns[i]) < min_length):
+			min_length = len(solns[i])
+			min_index = i
+	return solns[min_index]
+
+print("This program solves a countle-type problem. The program will output a solution which requires the fewest number of steps (there may be more than one such solution). It may take a while to find an answer.")
 numbers = list(map(int, input("Enter starting numbers (up to six values) separated by commas: ").split(',', 6)))
 sol = int(input("Enter final number: "))
 
-solution = solve(numbers, sol)
+solutions = solve(numbers, sol)
 
-if(len(solution) == 0):
-	print("No solution!")
+if(len(solutions) == 0):
+	print("No solutions!")
 else:
+	solution = find_best_solution(solutions)
 	for i in range(len(solution)-1, -1, -1):
-		print(solution[i])
+		for j in range(len(solution[i])):
+			print(solution[i][j], end='')
+		print()
 
 input()
